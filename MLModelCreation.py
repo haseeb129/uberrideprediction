771f69adffe08a4251291c2b0ec70616b2e7feb9
haseeb129 <63208659+haseeb129@users.noreply.github.com>
2020-04-15 17:25:16 +0500

    Add files via upload

---
+++ MLModelCreation.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+data = pd.read_csv("taxi.csv")
+y = data["Numberofweeklyriders"]
+X = data.drop("Numberofweeklyriders", axis=1)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, random_state=0)
+reg = LinearRegression()
+reg.fit(X_train, y_train)
+
+print(reg.score(X_train, y_train))
+print(reg.score(X_test, y_test))
+
+pickle.dump(reg, open('taxi.pk1', 'wb'))
+
+
+model = pickle.load(open('taxi.pk1', 'rb'))
+print(model.predict([[80, 1770000, 6000, 85]]))
+# conda list to view install pakages
---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify, render_template
+import numpy as np
+import pickle
+import math
+app = Flask(__name__)
+model = pickle.load(open('taxi.pk1', 'rb'))
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    listof_Values = [int(x) for x in request.form.values()]
+    arrayof_Values = [np.array(listof_Values)]
+    predictions = model.predict(arrayof_Values)
+
+    return render_template('index.html', predictions=math.floor(predictions[0]))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
